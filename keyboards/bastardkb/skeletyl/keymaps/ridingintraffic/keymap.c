307c909d6d42102317483e688c9b58b3f0b0fe3a
mike fettis <ridingintraffic@users.noreply.github.com>
2024-10-25 11:56:56 -0500

    goodbye wrappers

--- keyboards/bastardkb/skeletyl/keymaps/ridingintraffic/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/ridingintraffic/keymap.c
@@ -78,62 +78,37 @@
 #define __THUMBS_PL_3L_LOWER__       LT(2, KC_LCTL), KC_LALT, KC_BSPC
 #define __THUMBS_PL_3R_LOWER__      KC_SPC, KC_ENT, LT(3, KC_LALT)
 
-#define __BASE_NUMPAD_1_1__          KC_MPLY, KC_VOLD,   KC_VOLU,  KC_NO
-#define __BASE_NUMPAD_1_2__          ZMMT,    ZMSV,      ZMSS,     ZMSP
-#define __BASE_NUMPAD_1_3__          KC_CPYW,    KC_CPYP,      KC_NO,     KC_NO
-#define __BASE_NUMPAD_1_4__          KC_LCTL, KC_LGUI,   KC_LALT,  KC_NO
-
-#define __BASE_NUMPAD_2_1__          KC_7, KC_8,   KC_9,      KC_PLUS
-#define __BASE_NUMPAD_2_2__          KC_4, KC_5,   KC_6,      KC_MINUS
-#define __BASE_NUMPAD_2_3__          KC_1, KC_2,   KC_3,      KC_ENTER
-#define __BASE_NUMPAD_2_4__          KC_0, KC_DOT, LT(2,KC_ENTER), LT(1,KC_ENTER)
-
-#define __BASE_NUMPAD_3_1__          LCTL(KC_A), LCTL(KC_E),  KC_PGUP, KC_PGDN 
-#define __BASE_NUMPAD_3_2__          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-#define __BASE_NUMPAD_3_3__          KC_NO,   KC_NO,   KC_NO,   KC_NO
-#define __BASE_NUMPAD_3_4__          KC_NO,   KC_NO,   KC_NO,   KC_NO
-
-
-#define __RAISE_NUMPAD_1__
-#define __RAISE_NUMPAD_2__
-#define __RAISE_NUMPAD_3__
-#define __RAISE_NUMPAD_4__
-
-#define __LOWER_NUMPAD_1__
-#define __LOWER_NUMPAD_2__
-#define __LOWER_NUMPAD_3__
-#define __LOWER_NUMPAD_4__
 
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_split_3x5_3(
-   _________________QWERTY_L1_________________,              _________________QWERTY_R1_________________,  \
-   _________________QWERTY_L2_________________,              _________________QWERTY_R2_________________, \
-  _________________QWERTY_L3_________________,              _________________QWERTY_R3_________________,  \
-                                __THUMBS_3L__, __THUMBS_3R__
+KC_Q,    KC_W,  KC_E,    KC_R,                KC_T, KC_Y,    KC_U,                KC_I,          KC_O,          KC_P,
+LT(3,KC_A), LT(2,KC_S), LT(1,KC_D),    MT(MOD_LSFT, KC_F),    KC_G, KC_H,    MT(MOD_LSFT, KC_J),  LT(1,KC_K),    LT(2,KC_L),    LT(3,KC_QUOT),
+MT(MOD_LSFT, KC_Z),    KC_X,    KC_C,    KC_V,                KC_B, KC_N,    KC_M,                KC_COMM, KC_DOT,  MT(MOD_LSFT, KC_SLSH) 
+                                MT(MOD_LSFT, KC_UNDS), KC_LGUI, MT(MOD_MEH, KC_BSPC), MT(MOD_MEH, KC_SPC), MT(MOD_HYPR, KC_ENT), MT(MOD_LSFT, KC_MINUS)
 ),
 
   [1] = LAYOUT_split_3x5_3(
-   _________________RAISE_L1__________________,              _________________RAISE_R1__________________, \
-  _________________RAISE_L2__________________,             _________________RAISE_R2__________________, \
-  _________________RAISE_L3__________________,             _________________RAISE_R3__________________, \
-                                __THUMBS_3L_RAISE__, __THUMBS_3R_RAISE__
+QK_GESC, KC_VOLD, KC_VOLU, KC_LCBR, KC_LBRC,              KC_RBRC, KC_RCBR, KC_NO, KC_NO, KC_MPLY, 
+KC_GRV, KC_NO, KC_NO, KC_NO, KC_SCLN,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DQT, 
+KC_TILDE, _______, KC_CPYW, KC_CPYP, KC_LBRC,             KC_RBRC, KC_MINUS, KC_EQL, KC_PIPE,  KC_BSLS, 
+                                KC_LCTL, KC_LALT, KC_DEL, KC_TAB, KC_EQL, KC_PLUS
 ),
 
   [2] = LAYOUT_split_3x5_3(
- _________________LOWER_L1__________________,             _________________LOWER_R1__________________, \
- _________________LOWER_L2__________________,             _________________LOWER_R2__________________,  \
-_________________LOWER_L3__________________,             _________________LOWER_R3__________________, \
-                                __THUMBS_3L_LOWER__, __THUMBS_3R_LOWER__
+KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0, 
+RGB_TOG, RGB_RMOD, RGB_MOD, RGB_M_P, RGB_SAI,             _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_QUOT,  
+KC_LCBR, RGB_SAD, KC_CPYW, KC_CPYP,   RGB_SAD,             KC_RCBR, KC_HOME, KC_PGDN, KC_PGUP, KC_END, 
+                                LT(1, KC_LCTL), KC_LALT, KC_DEL,  MT(MOD_MEH, KC_SPC), KC_EQL, KC_PLUS
   ),
 
   [3] = LAYOUT_split_3x5_3(
-_________________ACTION_L1_________________,              _________________ACTION_R1_________________, \
-_________________ACTION_L2_________________,              _________________ACTION_R2_________________, \
-_________________ACTION_L3_________________,              _________________ACTION_R3_________________, \
-                                __THUMBS_3L_LOWER__, __THUMBS_3R_LOWER__
+KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN_, 
+RGB_VAI, RGB_HUI, RGB_SAI, RGB_MOD, RGB_VAI,              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_EQL, 
+RGB_VAD, RGB_HUD, RGB_SAD, RGB_RMOD, RGB_VAD,              _______, KC_LEFT, KC_DOWN, KC_UP,   KC_QUOT_, 
+                                LT(1, KC_LCTL), KC_LALT, KC_DEL,  MT(MOD_MEH, KC_SPC), KC_EQL, KC_PLUS
 
   )
 };
