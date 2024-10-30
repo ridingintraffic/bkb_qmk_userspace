2c9c41f857153dfb38981f2ec232689e10b93aa9
mike fettis <ridingintraffic@users.noreply.github.com>
2024-10-30 10:22:02 -0500

    gotta go left

--- keyboards/bastardkb/skeletyl/keymaps/ridingintraffic/config.h
+++ keyboards/bastardkb/skeletyl/keymaps/ridingintraffic/config.h
@@ -4,3 +4,10 @@
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define  QUICK_TAP_TERM 200
+#ifdef EE_HANDS
+    #undef EE_HANDS
+#endif
+#ifdef MASTER_RIGHT
+    #undef MASTER_RIGHT
+#endif
+#define MASTER_LEFT
